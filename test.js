69b0a59ce7b26fbb5d4dae017725d6736baeac78
km02615 <45234871+km02615@users.noreply.github.com>
2020-02-03 23:08:39 -0500

    Added while loop example

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
- var myAge = 26;
+ var age = 5;
 
- if (myAge < 18 || myAge > 30 || myAge === 25){
+ while (age < 10){
 
-    document.write('You aint coming!');
-
+console.log("Your age is less than 10");
+age++;
  }
 
- else{
-    document.write('You can come, you cool dude!');
- }
+ document.write("You are now over 10");
+ 
