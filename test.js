126ef8dba5cd6d5c91440e3d57dd0ff114a2431f
km02615 <45234871+km02615@users.noreply.github.com>
2020-02-03 22:56:06 -0500

    Added logical operators

--- test.js
+++ test.js
@@ -1,26 +1,11 @@
- 
+ var myAge = 26;
 
- var myAge = 55;
+ if (myAge < 18 || myAge > 30 || myAge === 25){
 
- if (myAge > 30){
+    document.write('You aint coming!');
 
- document.write("You are over 30!")
+ }
 
-}
-
-else if (myAge > 20){ 
- 
-    document.write("You are over 20!");
-
-}
-
-else if (myAge >10){
-
-    document.write("You are over 10!");
-
-}
-
-else {
-
-    document.write("you are not over 10!");
-} 
+ else{
+    document.write('You can come, you cool dude!');
+ }
