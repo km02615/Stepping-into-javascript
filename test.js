0ff5211d057ddc6ea0cf6835536592b23f2bb787
km02615 <45234871+km02615@users.noreply.github.com>
2020-02-18 14:36:39 -0500

    Used a for  loop to get links

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
- var age = 5;
-
- while (age < 10){
-
-console.log("Your age is less than 10");
-age++;
- }
-
- document.write("You are now over 10");
+ var links = document.getElementsByTagName("a");
  
+ for (i=1; i<= links.length; i++) {
+    console.log("This is is link number" + 1);
+
+ }
+ 
+ document.write("All links now looped");
